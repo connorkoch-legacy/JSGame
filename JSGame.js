760a0a96563746195c0f9b0e0eebf1cfcb6a339f
Thomas Parry <tparry@mines.edu>
2018-10-10 16:24:29 -0600

    background color for body and canvas

--- JSGame.js
+++ JSGame.js
@@ -1,29 +1,27 @@
-var c = document.getElementById("myCanvas")
+document.addEventListener("DOMContentLoaded", () => {
+  var c = document.getElementById("myCanvas")
+  initDraw(c)
+});
+function initDraw(c) {
 
-
-function initDraw() {
   var ctx = c.getContext("2d")
   ctx.canvas.width = window.innerWidth * 0.4
   ctx.canvas.height = ctx.canvas.width
   var sqSide = ctx.canvas.clientWidth / 4.5
 
-  ctx.fillStyle = "#060606"
-  ctx.fill()
+  ctx.fillStyle = "darkgray"
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
-  var xVals = []
-  var offset = ctx.canvas.width / 8
+  var offset = ctx.canvas.width / 22.5
 
   for (i = 0; i < 4; i++) {
     for (j = 0; j < 4; j++) {
-      drawSquare(offset*(i+1) + sqSide * i, offset*(j+1) + sqSide * j, sqSide)
+      drawSquare(offset*(i+1) + sqSide * i, offset*(j+1) + sqSide * j, sqSide, ctx)
     }
   }
-
 }
 
-function drawSquare(leftX, topY, side) {
-
-
+function drawSquare(leftX, topY, side, ctx) {
   ctx.fillStyle = "gray"
-  ctx.fillRect(leftX, topY, sqSide, side)
+  ctx.fillRect(leftX, topY, side, side)
 }
